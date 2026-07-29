2cdacbe142fac1c1876380cf5fcf6f2dd0199d6c
DeevsDeevs <49076636+DeevsDeevs@users.noreply.github.com>
2026-07-29 17:21:18 +0000

    chore: update pup to 1.10.0

--- package.nix
+++ package.nix
@@ -6,24 +6,24 @@
 }:
 
 let
-  version = "1.6.2";
+  version = "1.10.0";
 
   sources = {
     "aarch64-darwin" = {
       asset = "pup_${version}_Darwin_arm64.tar.gz";
-      hash = "sha256-er8nzA57pJbr667eWDdmWUC2nThWBor4lntnkGh/pvY=";
+      hash = "sha256-py1xuEeOU1XMZzF3ygBZKmBFcRfoPklPGdLNkIXe//s=";
     };
     "x86_64-darwin" = {
       asset = "pup_${version}_Darwin_x86_64.tar.gz";
-      hash = "sha256-dME8Xqby+BWVn3Go5WHEUTpuYOINvcCNMdCe0ILstEI=";
+      hash = "sha256-I2DBeZ25wZFnkdjW4niD4ArgMTknhXBw6AMHvFlF8Rg=";
     };
     "aarch64-linux" = {
       asset = "pup_${version}_Linux_arm64.tar.gz";
-      hash = "sha256-ADRtVg+3Eb0f3aU01bIQDe2Bae/1nnFsI1FVv3AcdvI=";
+      hash = "sha256-WP8yJnXJkvSJtGDoweVKfceO2lpoCv92PZlmmQhSPdY=";
     };
     "x86_64-linux" = {
       asset = "pup_${version}_Linux_x86_64.tar.gz";
-      hash = "sha256-7lAsWzx7PVZywOraiP25Y4+LgISfuP+6ai3p250pVy8=";
+      hash = "sha256-52jzgfIPxO3n61vFXOAVtUWFYgX0t9h7QNPHrqvUDas=";
     };
   };
 
